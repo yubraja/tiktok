134cbfb1dcbb006cb9121a2d41b8b1a4362f8809
yubraja <yubrajadhikari987@gmail.com>
2023-06-02 17:03:08 +0545

    home screen added

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -2,49 +2,67 @@ import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
-import '../constants.dart';
 import 'package:image_picker/image_picker.dart';
+import '../constants.dart';
 import '../models/user.dart' as model;
-
+import '../views/screens/home_screen.dart';
+import '../views/screens/auth/login_screen.dart';
 class AuthController extends GetxController {
   static AuthController instance = Get.find();
-
+  late Rx<User?> _user;
   late Rx<File?> _pickedImage;
 
   File? get profilePhoto => _pickedImage.value;
+  User get user => _user.value!;
+
+  @override
+  void onReady() {
+    super.onReady();
+    _user = Rx<User?>(firebaseAuth.currentUser);
+    _user.bindStream(firebaseAuth.authStateChanges());
+    ever(_user, _setInitialScreen);
+  }
+
+  _setInitialScreen(User? user) {
+    if (user == null) {
+      Get.offAll(() => LoginScreen());
+    } else {
+      Get.offAll(() => const HomeScreen());
+    }
+  }
 
   void pickImage() async {
     final pickedImage =
         await ImagePicker().pickImage(source: ImageSource.gallery);
-    Get.snackbar('Profile Picture', 'Sucessfully uploaded!!');
+    if (pickedImage != null) {
+      Get.snackbar('Profile Picture',
+          'You have successfully selected your profile picture!');
+    }
     _pickedImage = Rx<File?>(File(pickedImage!.path));
   }
 
-  //registering the user
-  //upload to firebase storage
+  // upload to firebase storage
   Future<String> _uploadToStorage(File image) async {
     Reference ref = firebaseStorage
         .ref()
         .child('profilePics')
         .child(firebaseAuth.currentUser!.uid);
 
-    TaskSnapshot snap = await ref.putFile(image);
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
     String downloadUrl = await snap.ref.getDownloadURL();
-    return downloadUrl; 
+    return downloadUrl;
   }
 
+  // registering the user
   void registerUser(
-    String username,
-    String email,
-    String password,
-    File? image,
-  ) async {
+      String username, String email, String password, File? image) async {
     try {
       if (username.isNotEmpty &&
           email.isNotEmpty &&
           password.isNotEmpty &&
           image != null) {
-        //save out user to our auth and firebase firestore
+        // save out user to our ath and firebase firestore
         UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
           email: email,
           password: password,
@@ -55,15 +73,15 @@ class AuthController extends GetxController {
           email: email,
           uid: cred.user!.uid,
           profilePhoto: downloadUrl,
-        );   
-
-        await fireStore.collection('users').doc(cred.user!.uid).set(user.toJson());
-        // .doc(cred.user!.uid)
-        // .set(user.toJson());
+        );
+        await fireStore
+            .collection('users')
+            .doc(cred.user!.uid)
+            .set(user.toJson());
       } else {
         Get.snackbar(
           'Error Creating Account',
-          'please enter all the fields!',
+          'Please enter all the fields',
         );
       }
     } catch (e) {
@@ -77,19 +95,23 @@ class AuthController extends GetxController {
   void loginUser(String email, String password) async {
     try {
       if (email.isNotEmpty && password.isNotEmpty) {
-
-        await firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
-        print('login sucess!!');
-        
+        await firebaseAuth.signInWithEmailAndPassword(
+            email: email, password: password);
       } else {
-        Get.snackbar('Incorrect Email or Password!!',
-            'Please enter correct email and password');
+        Get.snackbar(
+          'Error Logging in',
+          'Please enter all the fields',
+        );
       }
     } catch (e) {
       Get.snackbar(
-        'Error Creating Account',
+        'Error Loggin gin',
         e.toString(),
       );
     }
+  }
+
+  void signOut() async {
+    await firebaseAuth.signOut();
   }
 }
