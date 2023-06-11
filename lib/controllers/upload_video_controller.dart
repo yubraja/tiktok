6e022bbe3da25ecc2dd7f6993a16e4eb67507f83
yubraja <yubrajadhikari987@gmail.com>
2023-06-11 22:25:46 +0545

    video compressed

--- lib/controllers/upload_video_controller.dart
+++ lib/controllers/upload_video_controller.dart
@@ -4,31 +4,34 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:tiktok/constants.dart';
+import 'package:video_compress/video_compress.dart';
 
-class UploadVideoController extends GetxController{
-
-  _
-
-
-  _uploadVideoToStorage(String id, String videoPath){
-    Reference ref= firebaseStorage.ref().child('videos').child(id);
+class UploadVideoController extends GetxController {
+  _compressVideo(String videoPath) async  {
+    final compressedVideo= await VideoCompress.compressVideo(
+      videoPath,
+      quality: VideoQuality.MediumQuality,
+    );
+    return compressedVideo!.file;
   }
 
-  uploadVideo(String name, String caption ,String videoPath) async{
-    try{
+  Future<String> _uploadVideoToStorage(String id, String videoPath) async{
+    Reference ref = firebaseStorage.ref().child('videos').child(id);
+    UploadTask uploadTask=ref.putFile(_compressVideo(videoPath));
+    TaskSnapshot snap =await uploadTask;
+    String downloadUrl= await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
 
-      String uid=firebaseAuth.currentUser!.uid;
-      DocumentSnapshot userDoc=await fireStore.collection('users').doc(uid).get();
+  uploadVideo(String name, String caption, String videoPath) async {
+    try {
+      String uid = firebaseAuth.currentUser!.uid;
+      DocumentSnapshot userDoc =
+          await fireStore.collection('users').doc(uid).get();
 
-      var allDocs=await fireStore.collection('videos').get();
-      int len=allDocs.docs.length;
-      _uploadVideoToStorage('video $len',videoPath);
-
-
-    
-    }
-    catch(e){
-
-    }
+      var allDocs = await fireStore.collection('videos').get();
+      int len = allDocs.docs.length;
+      String videoUrl=await _uploadVideoToStorage('video $len', videoPath);
+    } catch (e) {}
   }
 }
