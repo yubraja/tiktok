2d5cd2cfc23147df5b65327ca3bf7293ae616ae2
yubraja <yubrajadhikari987@gmail.com>
2023-06-11 09:07:44 +0545

    uploadVideoToStorage function

--- lib/controllers/upload_video_controller.dart
+++ lib/controllers/upload_video_controller.dart
@@ -1,16 +1,29 @@
 import 'dart:html';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:tiktok/constants.dart';
 
 class UploadVideoController extends GetxController{
+
+  _
+
+
+  _uploadVideoToStorage(String id, String videoPath){
+    Reference ref= firebaseStorage.ref().child('videos').child(id);
+  }
 
   uploadVideo(String name, String caption ,String videoPath) async{
     try{
 
       String uid=firebaseAuth.currentUser!.uid;
       DocumentSnapshot userDoc=await fireStore.collection('users').doc(uid).get();
+
+      var allDocs=await fireStore.collection('videos').get();
+      int len=allDocs.docs.length;
+      _uploadVideoToStorage('video $len',videoPath);
+
 
     
     }
